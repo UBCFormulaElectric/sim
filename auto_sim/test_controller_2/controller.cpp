7e13aa90c5bbccf787ec53357f5b63139a71dc23
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-25 21:58:28 -0400

    stupid ahh qp solver
    This reverts commit ce240a7fe58fdd12ad532765e92d194b3011e2b7.

--- auto_sim/test_controller_2/controller.cpp
+++ auto_sim/test_controller_2/controller.cpp
@@ -1,40 +1,6 @@
 #include "controller.hpp"
-#include <proxsuite/proxqp/dense/dense.hpp>
-
-using namespace proxsuite::proxqp;
 
 ControlOutput compute(const VehicleState& ve) {
-    constexpr isize dim = 3, n_eq = 0, n_in = 3;
-    constexpr double eps_abs = 1e-9;
-
-    // qp
-    Eigen::Matrix<double, dim, dim> H;
-    Eigen::Vector<double, dim> g = Eigen::Vector<double, dim>::Random(dim);
-
-    // bounds constraints
-    Eigen::Matrix<double, n_in, dim> C;
-    Eigen::Vector<double, n_in> l;
-    l << -1.0, -1.0, -1.0;
-    Eigen::Vector<double, n_in> u;
-    u << 1.0, 1.0, 1.0;
-
-    // equality constraints
-    Eigen::Matrix<double, n_eq, dim> A;
-    Eigen::Vector<double, n_eq> b = Eigen::Vector<double, n_eq>::Zero(n_eq);
-
-    dense::QP<double> qp(dim, n_eq, n_in);
-    qp.settings.eps_abs = eps_abs;
-    qp.settings.initial_guess = InitialGuessStatus::NO_INITIAL_GUESS;
-    qp.settings.verbose = true;
-    qp.init(H, g, A, b, C, l, u);
-    qp.solve();
-
-    // std::cout << "primal residual: " << qp.results.info.pri_res << std::endl;
-    // std::cout << "dual residual: " << qp.results.info.dua_res << std::endl;
-    // std::cout << "total number of iteration: " << qp.results.info.iter
-    //           << std::endl;
-    // std::cout << "setup timing " << qp.results.info.setup_time << " solve time "
-    //           << qp.results.info.solve_time << std::endl;
-
+    // t.adj will contain the adj list
     return { 0, 0 };
 }
