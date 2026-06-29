25897ef48e7d901bf33a76797f6453426de10b24
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-28 22:15:13 -0400

    fuck me 3.11 ig

--- auto_sim/test_controller_2/forces/test_forces.py
+++ auto_sim/test_controller_2/forces/test_forces.py
@@ -1,56 +1,74 @@
 import numpy as np
-from test_controller_2.forces.forcespro_helper import forcespro
-import test_controller_2.forces.get_userid as get_userid
+from forcespro_helper import forcespro
+import get_userid
 
 N = 10
-nx = 0
-nu = 0
-R = np.zeros((nu,nu))
-P = np.zeros((nx,nx))
-Q = np.zeros((nx,nx))
-A = np.zeros((nx,nx))
-B = np.zeros((nx,nu))
-umin = np.zeros((nu,1))
-umax = np.zeros((nu,1))
-xmin = np.zeros((nx,1))
-xmax = np.zeros((nx,1))
+nx = 3 # state variable count
+nu = 2 # control variable count
 
-# FORCESPRO multistage form # assume variable ordering zi = [u{i-1}, x{i}] for i=1...N
+# costs on control
+R = np.zeros((nu,nu))
+# costs on state
+P = np.zeros((nx,nx)) # cost on state at final stage
+Q = np.zeros((nx,nx))
+
+A = np.zeros((nx,nx)) # transition on state
+B = np.zeros((nx,nu)) # transition on control
+
+umin = np.zeros(nu)
+umax = np.zeros(nu)
+xmin = np.zeros(nx)
+xmax = np.zeros(nx)
+
+# FORCESPRO multistage form
+# assume variable ordering zi = [u{i-1}, x{i}] for i=1...N
 stages = forcespro.MultistageProblem(N)
 
 # get stages struct of length N
 for i in range(N):
 	# dimensions
-	stages.dims[ i ]['n'] = nx+nu # number of stage variables
-	stages.dims[ i ]['r'] = nx    # number of equality constraints
-	stages.dims[ i ]['l'] = nx+nu # number of lower bounds
-	stages.dims[ i ]['u'] = nx+nu # number of upper bounds
-	
+	stages.dims[i]['n'] = nx+nu # number of stage variables (dimension of z)
+	stages.dims[i]['r'] = nx    # number of equality constraints
+	stages.dims[i]['l'] = nx+nu # number of lower bounds
+	stages.dims[i]['u'] = nx+nu # number of upper bounds
+	stages.dims[i]['p'] = 0     # number of polytopic constraints
+	stages.dims[i]['q'] = 0     # number of quadratic constraints
+
 	# cost
-	if ( i == N-1 ):
-		stages.cost[ i ]['H'] = np.vstack((np.hstack((R,np.zeros((nu,nx)))), np.hstack((np.zeros((nx,nu)),P))))
+	if i == N-1:
+		# R P blockmatrix
+		stages.cost[i]['H'] = np.vstack((
+			np.hstack((R,np.zeros((nu,nx)))),
+			np.hstack((np.zeros((nx,nu)),P))
+		))
 	else:
-		stages.cost[ i ]['H'] = np.vstack((np.hstack((R,np.zeros((nu,nx)))), np.hstack((np.zeros((nx,nu)),Q))))
+		# R Q blockmatrix
+		stages.cost[i]['H'] = np.vstack((
+			np.hstack((R,np.zeros((nu,nx)))),
+			np.hstack((np.zeros((nx,nu)),Q))
+		))
+	stages.cost[i]['f'] = np.zeros((nx+nu, 1)) # linear cost terms
 
-	stages.cost[ i ]['f'] = np.zeros((nx+nu,1)) # linear cost terms
-	# lower bounds
-	stages.ineq[ i ]['b']['lbidx'] = range(1,nu+nx+1) # lower bound acts on these indices
-	stages.ineq[ i ]['b']['lb'] = np.concatenate((umin,xmin),0) # lower bound for this stage variable
-	# upper bounds
-	stages.ineq[ i ]['b']['ubidx'] = range(1,nu+nx+1) # upper bound acts on these indices
-	stages.ineq[ i ]['b']['ub'] = np.concatenate((umax,xmax),0) # upper bound for this stage variable
+	# # lower bounds
+	stages.ineq[i]['b']['lbidx'] = range(1,nu+nx+1) # lower bound acts on these indices
+	stages.ineq[i]['b']['lb'] = np.concatenate((umin,xmin),0) # lower bound for this stage variable
+	# # upper bounds
+	stages.ineq[i]['b']['ubidx'] = range(1,nu+nx+1) # upper bound acts on these indices
+	stages.ineq[i]['b']['ub'] = np.concatenate((umax,xmax),0) # upper bound for this stage variable
 
-	# equality constraints
-	if ( i < N-1 ):
+	# # equality constraints
+	if i < N-1:
 		stages.eq[i]['C'] = np.hstack((np.zeros((nx,nu)),A))
-	if ( i>0 ):
-		stages.eq[i]['c'] = np.zeros((nx,1))
-	stages.eq[i]['D'] = np.hstack((B,-np.eye(nx)))
+	if i > 0:
+		stages.eq[i]['c'] = np.zeros(nx)
+	stages.eq[i]['D'] = np.hstack((B,-np.eye(nx))) # note that it is I rather than zeros, because equality constraint for state transition
 
 # RHS of first eq. constr. is a parameter: stages(1).eq.c = -A*x0
+# read 8.10
 stages.newParam('minusA_times_x0', [1], 'eq.c')
 # define output of the solver
-stages.newOutput('u0', 1, range(1,nu+1))
+# read 8.11
+stages.newOutput('u0', 1, range(1, nu + 1))
 
 codeoptions = forcespro.CodeOptions('solver_name')
 # codeoptions.platform = '' # to specify the target platform
